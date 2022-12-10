c17f61d4c002649f65dcae7fd400f8924e25881b
Varga <vargazsigmond02@gmail.com>
2022-12-10 17:55:15 +0100

    Controller

---
+++ src/main/java/hu/petrik/restclient/vargazsigmond_javafxrestclientdolgozat/UpdateSalaryController.java
@@ -0,0 +1,77 @@
+package hu.petrik.restclient.vargazsigmond_javafxrestclientdolgozat;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UpdateSalaryController extends Controller {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField salaryField;
+    @FXML
+    private Spinner<Integer> booleanField;
+    @FXML
+    private Button updateButton;
+
+    private Salary salary;
+
+    public void setPerson(Salary salary) {
+        this.salary = salary;
+        nameField.setText(this.salary.getName());
+        salaryField.setText(this.salary.getSalary());
+        booleanField.getValueFactory().setValue(this.salary.isDegree());
+    }
+
+    @FXML
+    private void initialize() {
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 200, 30);
+        ageField.setValueFactory(valueFactory);
+    }
+
+    @FXML
+    public void updateClick(ActionEvent actionEvent) {
+        String name = nameField.getText().trim();
+        String email = emailField.getText().trim();
+        int age = ageField.getValue();
+        if (name.isEmpty()) {
+            warning("Name is required");
+            return;
+        }
+        if (email.isEmpty()) {
+            warning("Email is required");
+            return;
+        }
+        // TODO: validate email format
+        this.person.setName(name);
+        this.person.setEmail(email);
+        this.person.setAge(age);
+        Gson converter = new Gson();
+        String json = converter.toJson(this.person);
+        try {
+            String url = App.BASE_URL + "/" + this.person.getId();
+            Response response = RequestHandler.put(url, json);
+            if (response.getResponseCode() == 200) {
+                Stage stage = (Stage) this.updateButton.getScene().getWindow();
+                stage.close();
+            } else {
+                String content = response.getContent();
+                error(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hu.petrik.restclient.vargazsigmond_javafxrestclientdolgozat {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-
-    opens hu.petrik.restclient.vargazsigmond_javafxrestclientdolgozat to javafx.fxml;
+    opens hu.petrik.restclient.vargazsigmond_javafxrestclientdolgozat to javafx.fxml, com.google.gson;
     exports hu.petrik.restclient.vargazsigmond_javafxrestclientdolgozat;
 }
